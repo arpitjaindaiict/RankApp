caf9213c114006239d9739376721e7b620063659
Arpit Jain <arpitkookada@gmail.com>
2011-11-05 13:21:09 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RankApp::Application.config.secret_token = '37ddc11de939ef4b1187dd48bd382b42852da2cf444c5a1a58a8b8fcf5ef421a010aef6a18e56df137a179eb98f926783c1ac55c7e3160273c76443b56a9be35'
